cd98377c0ce8fe440b6881bc39a29cc7d9943d3a
Mrinmayb11 <131666862+Mrinmayb11@users.noreply.github.com>
2023-06-06 21:11:24 +0530

    first commit

--- script.js
+++ script.js
@@ -1,79 +1,68 @@
 const TickBtns = document.getElementsByClassName("btnsI");
 const TickBtnsArr = Array.from(TickBtns);
 
-
 TickBtnsArr.forEach(btn => { 
-    let isChecked = false;
-    btn.addEventListener("click", (clickedBtn) => {  
-
+    btn.addEventListener("click", (clickedBtn) => {
         const Input = clickedBtn.target.nextElementSibling;
-        if(Input.value !== ""){
-            clickedBtn.target.classList.toggle("fa-solid");
-            clickedBtn.target.classList.toggle("fa-check");
-            
-        }else if(clickedBtn.target.classList.contains("fa-solid")){
-            clickedBtn.target.classList.remove("fa-solid", "fa-check");
+        const isChecked = btn.classList.contains("fa-solid");
+        
+        if (Input.value.trim() !== "") {
+            btn.classList.toggle("fa-solid");
+            btn.classList.toggle("fa-check");
+        } else if (isChecked) {
+            btn.classList.remove("fa-solid", "fa-check");
         }
+        
         const CheckedLine = clickedBtn.target.previousElementSibling;
         const InputLength = Input.value.length;
+        
         if (isChecked) {
             CheckedLine.style.width = "";
-            isChecked = false;
         } else {
             CheckedLine.style.width = `${InputLength + InputLength / 100 * 75}%`;
-            isChecked = true;
         }
     });
 });
-
-// Inputs 
 
 const Inputs = document.getElementsByClassName("inputs");
 const InputsArr = Array.from(Inputs);
 const RemoveTask = document.getElementsByClassName("remove");
 const RemoveTaskArr = Array.from(RemoveTask);
 
-
 InputsArr.forEach(Input => {
     Input.addEventListener("input", (CheckInput) => {
-        RemoveBtn = CheckInput.target.nextElementSibling;
-
-        if (CheckInput.target.value === "" ) {
+        let RemoveBtn = CheckInput.target.nextElementSibling;
+        if (CheckInput.target.value === "") {
             RemoveBtn.classList.remove("fa-regular", "fa-circle-xmark");
-
-        }else{
+        } else {
             RemoveBtn.classList.add("fa-regular", "fa-circle-xmark");
         }
-        console.log(Input.value.length)
-         RemoveBtn.onclick = () => {
-
+        RemoveBtn.onclick = () => {
             const SelectedBtn = Input.nextElementSibling;
             const RemoveTick = Input.previousElementSibling;
             const CheckedLine = Input.previousElementSibling.previousElementSibling;
             SelectedBtn.classList.remove("fa-regular", "fa-circle-xmark");
             Input.value = "";
-            // const InputLenght = Input.value.length;
-            if(RemoveTick.classList.contains("fa-solid")){
-                RemoveTick.classList.remove("fa-solid" , "fa-check");
+            if (RemoveTick.classList.contains("fa-solid")) {
+                RemoveTick.classList.remove("fa-solid", "fa-check");
                 CheckedLine.style.width = "";
             }
-          };
+        };
     });
-
 });
 
 const cleanBtn = document.getElementById("clean");
+let RemoveBtn;
 cleanBtn.addEventListener("click", () => {
-  InputsArr.forEach((Input) => {
-    const SelectedBtn = Input.nextElementSibling;
-    const RemoveTick = Input.previousElementSibling;
-    const CheckedLine = Input.previousElementSibling.previousElementSibling;
-    SelectedBtn.classList.remove("fa-regular", "fa-circle-xmark");
-    Input.value = "";
-    if(RemoveTick.classList.contains("fa-solid")){
-        RemoveTick.classList.remove("fa-solid" , "fa-check");
-        CheckedLine.style.width = "";
-    }
-  });
+    InputsArr.forEach((Input) => {
+        const SelectedBtn = Input.nextElementSibling;
+        const RemoveTick = Input.previousElementSibling;
+        const CheckedLine = Input.previousElementSibling.previousElementSibling;
+        SelectedBtn.classList.remove("fa-regular", "fa-circle-xmark");
+        Input.value = "";
+        if (RemoveTick.classList.contains("fa-solid")) {
+            RemoveTick.classList.remove("fa-solid", "fa-check");
+            CheckedLine.style.width = "";
+        }
+    });
 });
-
